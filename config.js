0b8e9c02d534d3f4af79353c547c45d1b0c9be2e
szq66 <1758417140@qq.com>
2021-10-11 09:10:33 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '阿离小可爱', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '1758417140@qq.com', // 发送者邮箱
+  pass: 'iarvuoonsvpybgbg', // 发送者邮箱MTP协议密码
+  to: '1758417140@qq.com', // 发送到谁，填邮箱
+  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
+  location: '101280704',  // 和风天气-珠海香洲区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
+  startDay: '2021-10-10', // 在一起的日期
+}
